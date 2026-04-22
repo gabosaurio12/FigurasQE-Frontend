d403349c5e765c21504d399f3576fec49b601773
Gabo-Mac <gabgonlo05@gmail.com>
2026-04-22 00:22:06 -0600

    Login

---
+++ Pages/Login.chstml.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FigurasQE_WebClient.Pages;
+
+public class LoginModel : PageModel
+{
+
+    [BindProperty]
+    [Required(ErrorMessage = "El correo es obligatorio")]
+    [EmailAddress(ErrorMessage = "Formato de correo inválido")]
+    public string Email { get; set; }
+
+
+    [BindProperty]
+    [Required(ErrorMessage = "La contraseña es obligatoria")]
+    public string Password { get; set; }
+
+    public void OnGet()
+    {
+
+    }
+
+    public async void OnPost()
+    {
+        var client = new HttpClient();
+
+        var response = await client.PostAsJsonAsync(
+            "http://localhost:3000/auth/login",
+            new { Email, Password }
+        );
+
+        Console.WriteLine(response);
+    }
+}
