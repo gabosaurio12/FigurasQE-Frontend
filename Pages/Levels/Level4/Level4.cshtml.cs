27ec725377c7f8ba606bbbd3de0cc152a4806810
Gabo-Mac <gabgonlo05@gmail.com>
2026-05-12 20:21:07 -0600

    levels structure

---
+++ Pages/Levels/Level4/Level4.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FigurasQE_WebClient.Pages;
+
+public class Level4Model : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
